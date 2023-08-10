f1b7648ea7832f0132531b1fdab9b07e6306b73d
Vadim Wictorovich <vsadnik199@gmail.com>
2023-08-10 14:02:30 +0300

    comit3

--- HomeWork11/HomeWork11/ViewController.swift
+++ HomeWork11/HomeWork11/ViewController.swift
@@ -8,52 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
-    @IBOutlet weak var outVolLevel: UILabel!
-    @IBOutlet weak var outProgressView: UIProgressView!
-    @IBOutlet weak var outTF: UITextField!
-    @IBOutlet weak var outSlider: UISlider!
-    @IBOutlet weak var outDatePicker: UIDatePicker!
-    @IBOutlet weak var outTimeLbl: UILabel!
-    @IBOutlet weak var outSwich: UISwitch!
-    //private var swichOn = true
+    @IBOutlet private var outVolLevel: UILabel!
+    @IBOutlet private var outProgressView: UIProgressView!
+    @IBOutlet private var outTF: UITextField!
+    @IBOutlet private var outSlider: UISlider!
+    @IBOutlet private var outDatePicker: UIDatePicker!
+    @IBOutlet private var outTimeLbl: UILabel!
+    @IBOutlet private var outSwich: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func sliderAction(_ sender: Any) {
+    @IBAction private func sliderAction(_ sender: Any) {
         outTF.text = String(outSlider.value)
         outProgressView.setProgress(outSlider.value, animated: true)
-        
     }
     
-    
-    @IBAction func setThisTimeAction(_ sender: Any) {
+    @IBAction private func setThisTimeAction(_ sender: Any) {
         let timeStartFormatter = DateFormatter()
         timeStartFormatter.dateFormat = "HH:mm"
         outTimeLbl.text = timeStartFormatter.string(from: outDatePicker.date)
-        
     }
     
-    
-    @IBAction func swichAction(_ sender: Any) {
-        let isOn = outSwich.isOn
-        if  outSwich.isOn {
+    @IBAction private func swichAction(_ sender: Any) {
+        if outSwich.isOn {
             outTimeLbl.backgroundColor = #colorLiteral(red: 0.7372549176, green: 0.7372549176, blue: 0.7372549176, alpha: 1)
         } else {
             outTimeLbl.backgroundColor = .red
         }
-        
     }
         
-        
-    
-    
-    @IBAction func clearButAction(_ sender: Any) {
+    @IBAction private func clearButAction(_ sender: Any) {
         outTimeLbl.text = ""
     }
 }
-
-
